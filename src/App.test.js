4a32c8ab5feb9a0a63f29ce8aecee102f618a329
Alexandre DERNIAME <kiralex1995@gmail.com>
2017-04-12 20:47:14 +0200

    Dummy units to test enzyme

--- src/App.test.js
+++ src/App.test.js
@@ -1,3 +1,16 @@
 import React from 'react';
-import { shallow } from 'enzyme';
 import App from './App';
+import MenuItem from './MenuItem';
+import { shallow } from 'enzyme';
+
+it('renders three <Foo /> components', () => {
+  const wrapper = shallow(<App />);
+  console.log(wrapper.find(MenuItem).length);
+});
+
+describe('test test', () => {
+  it('tototo', () => {
+    const wrapper = shallow(<div>toto</div>);
+    expect(wrapper.equals(<div>toto</div>)).toBe(false);
+  });
+});
